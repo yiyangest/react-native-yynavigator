b4686866b75bf6431200196f27cb78f396ffdde6
yiyang <yiyangyuan@creditease.cn>
2016-02-23 16:51:16 +0800

    add missing comma

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var YYNavigator = {
     get NavigationController() {return require('./Navigation/NavigationController');},
-    get NavigationButton() {return require('./Navigation/NavigationButton');}
+    get NavigationButton() {return require('./Navigation/NavigationButton');},
     get NavigatorMixin() { return require('./Navigation/NavigatorMixin');}
 }
 
