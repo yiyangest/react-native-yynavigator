e4c3a8d67da0ad6602ae160d5a86d3d4717f20af
yiyang <yiyangyuan@creditease.cn>
2016-02-23 16:29:16 +0800

    fix typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var YYNavigator = {
-    get NavigationController() {return require('../Navigation/NavigationController');},
+    get NavigationController() {return require('./Navigation/NavigationController');},
     get NavigationButton() {return require('./Navigation/NavigationButton');}
     get NavigatorMixin() { return require('./Navigation/NavigatorMixin');}
 }
